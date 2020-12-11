26672018540ac2075e452ef30b1b32d6a29a52f2
Murat Baskıcıoğlu <muratbaskicioglu@gmail.com>
2020-12-11 21:41:57 +0300

    perf: switch server to Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
-import { NestFactory } from '@nestjs/core';
+import { AbstractHttpAdapter, NestFactory } from '@nestjs/core';
+import { INestApplication } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
+export async function createApp<T>(
+  httpAdapter?: AbstractHttpAdapter,
+): Promise<T | INestApplication> {
+  let app: INestApplication;
+
+  if (httpAdapter) {
+    app = await NestFactory.create(AppModule, httpAdapter);
+  } else {
+    app = await NestFactory.create(AppModule);
+  }
+
+  return app;
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await createApp<NestFastifyApplication>(new FastifyAdapter());
   await app.listen(3000);
 }
 bootstrap();
